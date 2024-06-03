e04258dffc43d50e8d45421d436f146a03c94472
Francisco Perez <francisco.perez.tr@gmail.com>
2024-06-03 18:01:42 -0300

    js

---
+++ clase13-introjs/js/main.js
@@ -0,0 +1,75 @@
+/* INTRO JS */
+
+// /* hola mundo desde archivo externo*/
+// console.log("Hola mundo");
+
+// //////variables
+// var nombre = "Francisco";
+
+// console.log(nombre);
+
+// let nombreLet = "Nicolas";
+
+// console.log(nombreLet);
+
+// let edad = 31; //declara variable numerica
+
+// const PI = 3.14; //declara constante
+
+// let tenerMascotas = true;
+
+// console.log(edad, PI, tenerMascotas);
+
+// //////ejercicio:
+
+// let Nombre = "Francisco";
+// let Apellido = "Perez";
+// let Edad = 31;
+// let tenesMascota = true;
+
+// console.log("Nombre:", Nombre, "Apellido:", Apellido, "Edad:", Edad, "Tenes mascota?:", tenesMascota);
+
+
+// //////operaciones basicas
+
+// let numero1 = 2;
+// let numero2 = 4;
+// let resultado = numero1 + numero2;
+// console.log(resultado);
+
+// let names = "Jose";
+// let surname = "Lopez";
+// const ESPACIO = " ";
+// let allName = names + ESPACIO + surname;
+// console.log(allName);
+
+// /////prompt
+
+// let entrada = prompt("Ingrese su nombre");
+// let salida = "Hola " + entrada;
+// alert(salida);
+
+// let numerox = parseInt(prompt("Ingrese su numero"));
+// const NUMEROB = 30;
+// let salidax = numerox + NUMEROB;
+// alert(salidax);
+
+// let nombree = prompt("Ingrese su nombre");
+// let apellidoo = prompt("Ingrese su apellido");
+// const ESPACIOO = " ";
+// let nomapell = nombree + ESPACIOO + apellidoo; 
+// alert(nomapell);
+
+let arrayTest = ["Francisco", "Sofia", "Nicolas", "Lucia", "Belen"];
+console.log(arrayTest);
+console.log(arrayTest.length);
+console.log(arrayTest[0]);
+
+let producto = {
+    nombre: "Taza",
+    precio: 300,
+    color: "Azul"
+};
+console.log(producto.nombre); 
+console.log(producto.precio);
+console.log(producto.color);
